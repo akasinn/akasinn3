c3705a417bd2f1a405413ca1eb526033ef5eaca4
akasinn <94238659+akasinn@users.noreply.github.com>
2022-12-08 21:00:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const version=82;
+const version=83;
 
 
 const canvas = document.getElementById("canvas");
@@ -35,9 +35,9 @@ const draw = () => {
     100
   );
   btx.fillStyle = "black";
-  btx.fillText("x",1025,975);
+  btx.fillText("x",1075,1000);
   btx.fillText("y",75,30);
-  btx.fillText("o",25,1070);
+  btx.fillText("o",25,1080);
   btx.save();
 
   //座標変換
@@ -49,15 +49,15 @@ const draw = () => {
   btx.moveTo(-40,5);
   btx.lineTo(-5,5);
   btx.lineTo(-5,1000);
-  btx.lineTo(-40,1000);
+  btx.lineTo(-20,1000);
   btx.lineTo(0,1040);
-  btx.lineTo(40,1000);
+  btx.lineTo(20,1000);
   btx.lineTo(5,1000);
   btx.lineTo(5,5);
   btx.lineTo(1000,5);
-  btx.lineTo(1000,40);
+  btx.lineTo(1000,20);
   btx.lineTo(1040,0);
-  btx.lineTo(1000,-40);
+  btx.lineTo(1000,-20);
   btx.lineTo(1000,-5);
   btx.lineTo(5,-5);
   btx.lineTo(5,-40);
@@ -202,5 +202,10 @@ xyoの文字を追加
 ver81
 xyoが上下反転していたので修正
 ver82
-xyoを位置を修正
+xyoの位置を修正
+ver83
+xyoの位置を修正、矢印を尖らせる
+
+
+
 */
